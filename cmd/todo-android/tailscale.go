7800a6689bd18f97decbaf26321d2b6265d5b238
Tarun Pothulapati <tarunpothulapati@outlook.com>
2023-05-24 19:36:56 +0530

    feat(android): add app from tailscale-android

---
+++ cmd/todo-android/tailscale.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"gioui.org/layout"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func getTailScaleServer() string {
+	// List all the servers on a tag
+	return "todo"
+}
+
+func (ui *UI) layoutOAuth(gtx layout.Context) layout.Dimensions {
+	// Define the input fields.
+	var email, password widget.Editor
+	email.SingleLine, password.SingleLine = true, true
+	email.Submit, password.Submit = true, true
+
+	// Define the sign-in button.
+	signin := material.Button(ui.theme, nil, "Sign in")
+
+	// Layout the input fields and sign-in button.
+	return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+		layout.Rigid(func(gtx C) D {
+			return material.Editor(ui.theme, &email, "Email").Layout(gtx)
+		}),
+		layout.Rigid(func(gtx C) D {
+			return material.Editor(ui.theme, &password, "Password").Layout(gtx)
+		}),
+		layout.Rigid(func(gtx C) D {
+			return signin.Layout(gtx)
+		}),
+	)
+}
